2c070c86ec0c73a9043c3c982a1f75562d5b064a
Nikhil85 <nikhilsanghvi85@gmail.com>
2016-09-02 11:31:05 +0530

    CRUDOperations

---
+++ CRUDOperations/target/user.sql
@@ -0,0 +1,45 @@
+/*
+SQLyog Ultimate v10.00 Beta1
+MySQL - 5.7.13-log : Database - user_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`user_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `user_db`;
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(100) DEFAULT NULL,
+  `lastName` varchar(100) DEFAULT NULL,
+  `sex` varchar(100) DEFAULT NULL,
+  `emailId` varchar(100) DEFAULT NULL,
+  `phoneNo` varchar(100) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+/*Data for the table `users` */
+
+LOCK TABLES `users` WRITE;
+
+insert  into `users`(`id`,`firstName`,`lastName`,`sex`,`emailId`,`phoneNo`,`address`) values (6,'Nikhil','Sanghi','Male','sanghvi.nikhil10@gmail.com','7898409833','100,Vyankteash Nagar,Indore');
+
+UNLOCK TABLES;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
